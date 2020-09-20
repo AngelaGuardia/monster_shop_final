6edacd3df066832816ba0efa98b0e7d5d22de5cf
Angela Guardia <angelaguardia20@gmail.com>
2020-09-19 19:31:31 -0700

    Create MVC files for Discounts Index Page access

--- spec/features/merchant/discounts/index_spec.rb
+++ spec/features/merchant/discounts/index_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
-describe 'As a merchant on my dashboard' do
-  describe 'I see a link to my discounts' do
-    
+describe 'As a merchant' do
+  describe 'on my dashboard' do
+    it 'I see a link to my discounts' do
+      merchant_store = create(:merchant)
+      merchant_employee = create(:merchant_user, merchant_id: merchant_store.id)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_employee)
+
+      visit merchant_dashboard_path
+
+      click_on "My Discounts"
+
+      expect(current_path).to eq(merchant_discounts_path)
+    end
   end
 end
