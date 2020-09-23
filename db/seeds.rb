7d0b716b174ec21060ac3d1348d1d863efaa4e0d
Angela Guardia <angelaguardia20@gmail.com>
2020-09-23 14:28:53 -0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# require 'factory_bot_rails'
+include FactoryBot::Syntax::Methods
 # require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
